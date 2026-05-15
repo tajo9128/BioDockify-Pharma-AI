591f07c40e995d48026dada9232930b67539e25a
tajo9128 <tajo9128@gmail.com>
2026-05-15 08:17:11 +0530

    Restore canvas as side-panel desktop: original rail+shell+tabs + taskbar, remove docking/3d-viewer

--- extensions/webui/right_canvas_register_surfaces/register-3d-viewer.js
+++
@@ -1,3 +0,0 @@
-﻿export default async function register3dViewer(canvas) {
-  canvas.registerSurface({ id: "viewer3d", title: "3D Viewer", icon: "view_in_ar", order: 24, modalPath: "/components/3d-viewer/3d-viewer-panel.html", open(){}, close(){} });
-}
